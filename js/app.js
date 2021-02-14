f1a47ed4dda390f79519800b87787ece6bec7a33
sukaynaghnemat <sukaynaghnemat@yahoo.com>
2021-02-15 00:50:42 +0200

    change

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
  
-let hours=['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:','Total:'];
+let hours=['6AM:','7AM:','8AM:','9AM:','10AM:','11AM:','12PM:','1PM:','2PM:','3PM:','4PM:','5PM:','6PM:','7PM:','8PM:'];
 
  function randomValue(min, max) {
 
